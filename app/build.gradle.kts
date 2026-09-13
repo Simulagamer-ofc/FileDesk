31967a14258a6c5c111ded88921fd0b4ac07c229
Simulagamer-ofc <eddy.edson2012@gmail.com>
2026-09-13 10:19:54 -0300

    chore: bump FileDesk to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android"); id("org.jetbrains.kotlin.plugin.compose") }
 android {
  namespace="com.simulagamer.filedesk"; compileSdk=35
- defaultConfig { applicationId="com.simulagamer.filedesk"; minSdk=26; targetSdk=35; versionCode=11; versionName="1.1.0" }
+ defaultConfig { applicationId="com.simulagamer.filedesk"; minSdk=26; targetSdk=35; versionCode=12; versionName="1.2.0" }
  compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
  kotlinOptions { jvmTarget="17" }
  buildFeatures { compose=true }
