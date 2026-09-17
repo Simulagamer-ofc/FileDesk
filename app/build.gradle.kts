020ff72ce4814e10e885179bf929df94a702abac
Simulagamer-ofc <eddy.edson2012@gmail.com>
2026-09-17 11:59:28 -0300

    feat: show user folders in Explorer content pane

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android"); id("org.jetbrains.kotlin.plugin.compose") }
 android {
  namespace="com.simulagamer.filedesk"; compileSdk=35
- defaultConfig { applicationId="com.simulagamer.filedesk"; minSdk=26; targetSdk=35; versionCode=15; versionName="1.5.0-beta1" }
+ defaultConfig { applicationId="com.simulagamer.filedesk"; minSdk=26; targetSdk=35; versionCode=16; versionName="1.6.0-beta1" }
  compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
  kotlinOptions { jvmTarget="17" }
  buildFeatures { compose=true }
